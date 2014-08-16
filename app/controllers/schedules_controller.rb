cffcc61adaed7d47d0fc8dadd354ada4bbb2db9f
John Keith <johnkeith@outlook.com>
2014-08-16 17:41:50 -0400

    schedules controller show_current method; current_day helper in app.rb

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,4 +1,7 @@
 class SchedulesController < ApplicationController
+  before_action :authenticate_user!
+
   def show_current
+    @schedule = Schedule.where(user_id: current_user.id, day_of_week: current_day)
   end
 end
