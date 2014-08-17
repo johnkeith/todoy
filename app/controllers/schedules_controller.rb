9fbc3a0ca6c698a540014d620160f8f5d1cd3b2b
John Keith <johnkeith@outlook.com>
2014-08-17 11:35:21 -0400

    add flash to schedules controller; button on show_current if no @schedule

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -3,5 +3,10 @@ class SchedulesController < ApplicationController
 
   def show_current
     @schedule = Schedule.where(user_id: current_user.id, day_of_week: current_day)
+    
+    if @schedule.empty?
+      flash[:alert] = "You have no schedules at the current moment.
+        Try creating a new one!"
+    end
   end
 end
