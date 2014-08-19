112eb6314727fdc119438b5037f67cda2739f30d
John Keith <johnkeith@outlook.com>
2014-08-19 11:26:09 -0400

    styling for single day view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
   root 'home#home'
+  get 'schedule/:day', to: "schedules#one_day", as: "schedule_for_day"
   get 'schedule', to: "schedules#show_current"
-  get 'schedule/:day', to: "schedules#one_day"
+
 end
